4422ba62c1d2622b080612c41783fd64e1bcc658
Jonathan Dupré <hello@jonathandupre.com>
2018-01-21 22:34:05 -0500

    continue writing tests

--- test/connected.js
+++ test/connected.js
@@ -1,14 +1,42 @@
 /* daemon connects to the api */
 
-// var test = require('tape')
+var test = require('tape')
 
-// test('there are non uploaded files: upload files', t => {
-//   t.fail('file is on the server')
-//   t.end()
-// })
+test('there are non uploaded files: upload files', t => {
+  t.plan(1)
 
-// test('a synced playlist is missing files: download files', t => {
-//   t.fail('all files are available locally')
-//   t.fail('index of files lists those files')
-//   t.end()
-// })
+  // log in
+  // lib tries uploading the file (through a stub)
+  // stub returns unexpected result
+  // lib continues trying to upload the file
+  // stub returns expected result
+  // status of track has changed somehow
+  // maybe track contains list of uploaded track ids
+
+  t.equal(uploadedFile.name, fileName, 'file is on the server')
+  t.end()
+
+  tracks.filter(keepNotUploaded)
+
+  function keepNotUploaded (track) {
+    return track.files.reduce()
+  }
+
+  functionn  (notUploaded) {
+    var filepath = notUploaded.filepath
+    var file = File.load(filepath)
+    try {
+      var uploadedFile = await upload(file)
+    } catch (err) {
+      t.fail('failed to upload file', err)
+    }
+    t.equal(uploadedFile.name, fileName, 'file is on the server')
+    t.end()
+  }
+})
+
+test('a synced playlist is missing files: download files', t => {
+  t.fail('all files are available locally')
+  t.fail('index of files lists those files')
+  t.end()
+})
--- test/inbox.js
+++ test/inbox.js
@@ -12,6 +12,25 @@ var config = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '../config.yaml'
 
 var app
 
+function test (description, fn) {
+  tape(description, function (t) {
+    setup()
+    fn(t)
+    teardown()
+  })
+}
+
+function setup () {
+  app = App(services, {
+    appdir: os.homedir() + '/__VibedriveTEST__',
+    user: config.user
+  })
+}
+
+function teardown () {
+  app.quit()
+}
+
 test('file is unsupported: move file to the unsupported folder', t => {
   t.plan(1)
 
@@ -52,32 +71,25 @@ test('file is supported: create a new track', t => {
   var fileName = 'audio-file.mp3'
   var waiting = path.join(app.folder.appdir, app.folder.subfolders.waiting)
   var inbox = path.join(app.folder.appdir, app.folder.subfolders.inbox)
+  var waitingWatcher = fs.watch(waiting)
 
-  cp(fileName, inbox, function () {
-    setTimeout(function () {
+  waitingWatcher.on('change', onWaitingChange)
+
+  cp(fileName, inbox)
+
+  var timer = setTimeout(function () {
+    t.fail('file should be in waiting folder in less than 3 seconds')
+  }, 3000)
+
+  function onWaitingChange (eventType, changedFile) {
+    if (changedFile === fileName) {
+      clearTimeout(timer)
+      waitingWatcher.close()
+      t.pass('file is in waiting folder')
+
+      services.dataStore.get('track', fileName)
       t.fail('track exits')
       t.fail('file is on the server')
-      t.ok(fs.readdirSync(waiting).includes(fileName), 'file is in waiting folder')
-    }, 1000)
-  })
-  t.end()
+    }
+  }
 })
-
-function test (description, fn) {
-  tape(description, function (t) {
-    setup()
-    fn(t)
-    teardown()
-  })
-}
-
-function setup () {
-  app = App(services, {
-    appdir: os.homedir() + '/__VibedriveTEST__',
-    user: config.user
-  })
-}
-
-function teardown () {
-  app.quit()
-}
