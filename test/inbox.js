5cd3c30e1e1ddb10f8c1923804701f6933e18a22
Jonathan Dupré <hello@jonathandupre.com>
2018-01-23 21:52:16 -0500

    moving forward w tests@

--- test/inbox.js
+++ test/inbox.js
@@ -2,94 +2,112 @@
 
 var os = require('os')
 var fs = require('fs')
-var cp = require('node-cp')
+var rimraf = require('rimraf')
 var yaml = require('js-yaml')
 var path = require('path')
-var tape = require('tape')
+var test = require('tape')
 var services = require('vibedrive-sdk')
 var App = require('../.')
-var config = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '../config.yaml')))
+var { user } = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '../config.yaml')))
+var sleep = require('hypno')
+var appdir = os.homedir() + '/__VibedriveTEST__'
 
-var app
-
-function test (description, fn) {
-  tape(description, function (t) {
-    setup()
-    fn(t)
-    teardown()
-  })
+function setupApp () {
+  return App(services, { appdir, user})
 }
 
-function setup () {
-  app = App(services, {
-    appdir: os.homedir() + '/__VibedriveTEST__',
-    user: config.user
-  })
-}
+// test('file is unsupported: move file to the unsupported folder', t => {
+//   t.plan(1)
 
-function teardown () {
-  app.quit()
-}
+//   rimraf.sync(appdir)
 
-test('file is unsupported: move file to the unsupported folder', t => {
-  t.plan(1)
+//   var app = setupApp()
 
-  const fileName = 'file.txt'
-  var filePath = path.join(app.folder.appdir, app.folder.subfolders.inbox, fileName)
-  var unsupported = path.join(app.folder.appdir, app.folder.subfolders.unsupported)
+//   app.on('ready', async function () {
+//     const fileName = 'file.txt'
 
-  fs.writeFileSync(filePath, 'whatever')
+//     var filePath = path.join(app.folder.subfolder('inbox'), fileName)
+//     var unsupported = app.folder.subfolder('unsupported')
 
-  setTimeout(function () {
-    var files = fs.readdirSync(unsupported)
+//     fs.writeFileSync(filePath, 'whatever')
 
-    t.ok(files.includes(fileName), 'file is now in unsupported folder')
-    t.end()
-  }, 1000)
-})
+//     await sleep(1000)
+
+//     var files = fs.readdirSync(unsupported)
+
+//     t.equal(files.includes(fileName), true, 'file is now in unsupported folder')
+//     t.end()
+//     app.quit()
+//   })
+// })
 
 test('file is a zip: unzip file and move it to the archive folder', t => {
-  t.plan(2)
+  rimraf.sync(appdir)
 
-  var fileName = 'test.zip'
-  var inbox = path.join(app.folder.appdir, app.folder.subfolders.inbox)
-  var archives = path.join(app.folder.appdir, app.folder.subfolders.archives)
+  var app = setupApp()
 
-  cp(fileName, inbox, function () {
-    setTimeout(function () {
-      t.ok(fs.readdirSync(archives).includes(fileName), 'file is in archive folder')
-      t.ok(fs.readdirSync(inbox).includes('unzipped.txt'), 'unzipped file is now in inbox')
-    }, 1000)
+  app.on('ready', function () {
+    var zippedFileName = 'archive.zip'
+    var unzippedFileName = 'unzipped.txt'
+
+    var inbox = app.folder.subfolder('inbox')
+    var archives = app.folder.subfolder('archives')
+
+    var inboxWatcher = fs.watch(inbox)
+    var archivesWatcher = fs.watch(archives)
+
+    inboxWatcher.on('change', function (eventName, changedFile) {
+      if (changedFile === zippedFileName) { return t.pass('archive is moved to inbox') }
+      if (changedFile === unzippedFileName) { return t.pass('archive is unzipped') }
+    })
+
+    archivesWatcher.on('change', function (eventName, changedFile) {
+      if (changedFile === zippedFileName) {
+        t.pass('archive is moved to archives')
+        app.quit()
+        t.end()
+      }
+    })
+
+    var src = path.join(__dirname, zippedFileName)
+    var dest = path.join(inbox, zippedFileName)
+
+    fs.writeFileSync(dest, fs.readFileSync(src))
+    t.equal(fs.existsSync(dest), true, 'file was copied to inbox')
   })
-
-  t.end()
 })
 
-test('file is supported: create a new track', t => {
-  t.plan(3)
+// test('file is supported: create a new track', t => {
+//   t.plan(3)
 
-  var fileName = 'audio-file.mp3'
-  var waiting = path.join(app.folder.appdir, app.folder.subfolders.waiting)
-  var inbox = path.join(app.folder.appdir, app.folder.subfolders.inbox)
-  var waitingWatcher = fs.watch(waiting)
+//   var app = setupApp()
 
-  waitingWatcher.on('change', onWaitingChange)
+//   var fileName = 'audio-file.mp3'
+//   var waiting = path.join(app.folder.appdir, app.folder.subfolders.waiting)
+//   var inbox = path.join(app.folder.appdir, app.folder.subfolders.inbox)
+//   var waitingWatcher = fs.watch(waiting)
 
-  cp(fileName, inbox)
+//   waitingWatcher.on('change', onWaitingChange)
 
-  var timer = setTimeout(function () {
-    t.fail('file should be in waiting folder in less than 3 seconds')
-  }, 3000)
+//   cp(fileName, inbox)
 
-  function onWaitingChange (eventType, changedFile) {
-    if (changedFile === fileName) {
-      clearTimeout(timer)
-      waitingWatcher.close()
-      t.pass('file is in waiting folder')
+//   var timeout = setTimeout(function () {
+//     clearTimeout(timeout)
+//     t.fail('file should be in waiting folder in less than 3 seconds')
+//     app.quit()
+//     app = null
+//   }, 3000)
 
-      services.dataStore.get('track', fileName)
-      t.fail('track exits')
-      t.fail('file is on the server')
-    }
-  }
-})
+//   function onWaitingChange (eventType, changedFile) {
+//     if (changedFile === fileName) {
+//       clearTimeout(timeout)
+//       waitingWatcher.close()
+//       t.pass('file is in waiting folder')
+
+//       services.dataStore.get('track', fileName)
+//       t.fail('track exits')
+//       t.fail('file is on the server')
+//       app.quit()
+//     }
+//   }
+// })
